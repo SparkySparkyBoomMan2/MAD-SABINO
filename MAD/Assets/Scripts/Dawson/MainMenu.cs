5afe4fd53c400b1b55f42852bfab18780886302b
Dawson Hill <extremedawson@gmail.com>
2022-02-09 21:20:57 -0800

    Quick UI Update
     - Added functionality to existing menus.
     - Created scripts for each menu.
     - Hooked Task Selector Menu into an existing scene.
     - Changed names of various items.

---
+++ MAD/Assets/Scripts/Dawson/MainMenu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TaskSelector(string task)
+    {
+        SceneManager.LoadScene(task);
+    }
+
+    public void OpenOptions()
+    {
+
+    }
+
+    public void CloseOptions()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quitting...");
+    }
+}
+
