34ebe63b9fa9f2d54741a88444c7da2244f9d5dd
SparkySparkyBoomMan2 <78051773+SparkySparkyBoomMan2@users.noreply.github.com>
2022-02-10 04:37:37 -0800

    Developer Arm Controls Done

---
+++ MAD/Assets/Scripts/Miguel/ArmAbstraction.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmAbstraction : MonoBehaviour
+{
+    public GameObject rightRig, leftRig;
+
+    private RotateThatThang rUpper, rLower, rHand;  // Scripts For Right
+    private RotateThatThang Upper, Lower, Hand;     // Scripts For Left
+    private GameObject go_rUp, go_rLow, go_rHand;   // Right
+    private GameObject go_Up, go_Low, go_Hand;      // Left
+
+    private enum Part { up, low, hand}
+    private Part part = Part.up;
+
+    void Start()
+    {
+        go_rUp = rightRig.transform.Find("rUpperControl").gameObject;
+        rUpper = go_rUp.GetComponent<RotateThatThang>();
+        go_rLow = rightRig.transform.Find("rLowerControl").gameObject;
+        rLower = go_rLow.GetComponent<RotateThatThang>();
+        go_rHand = rightRig.transform.Find("rHandControl").gameObject;
+        rHand = go_rHand.GetComponent<RotateThatThang>();
+
+        go_Up = leftRig.transform.Find("UpperControl").gameObject;
+        Upper = go_Up.GetComponent<RotateThatThang>();
+        go_Low = leftRig.transform.Find("LowerControl").gameObject;
+        Lower = go_Low.GetComponent<RotateThatThang>();
+        go_Hand = leftRig.transform.Find("HandControl").gameObject;
+        Hand = go_Hand.GetComponent<RotateThatThang>();
+    }
+
+    private void MatchRotation()
+    {
+        go_rUp.transform.localRotation = go_Up.transform.localRotation = Quaternion.identity;
+        go_rLow.transform.localRotation = go_Low.transform.localRotation = Quaternion.identity;
+        go_rHand.transform.localRotation = go_Hand.transform.localRotation = Quaternion.identity;
+    }
+
+    private void PartInit(Part part)
+    {
+        if (part == Part.up)
+        {
+            rUpper.Init();
+            Upper.Init();
+        }
+        else if (part == Part.low)
+        {
+            rLower.Init();
+            Lower.Init();
+        }
+        else if (part == Part.hand)
+        {
+            rHand.Init();
+            Hand.Init();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))        // Left Arm Only
+        {
+            rightRig.SetActive(false);
+            leftRig.SetActive(true);
+            Upper.rightControl = Lower.rightControl = Hand.rightControl = true;
+
+            PartInit(part);
+        }
+        else if (Input.GetKeyDown(KeyCode.X))   // Both Arms
+        {
+            rightRig.SetActive(true);
+            leftRig.SetActive(true);
+            Upper.rightControl = Lower.rightControl = Hand.rightControl = false;
+
+            PartInit(part);
+
+            // Code to match rotations, could have slicker code later
+            MatchRotation();
+        }
+        else if (Input.GetKeyDown(KeyCode.C))   // Right Arm Only
+        {
+            rightRig.SetActive(true);
+            leftRig.SetActive(false);
+            Upper.rightControl = Lower.rightControl = Hand.rightControl = false;
+
+            PartInit(part);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))       // Upper
+        {
+            rLower.enabled = Lower.enabled = false;
+            rHand.enabled = Hand.enabled = false;
+            rUpper.enabled = Upper.enabled = true;
+
+            part = Part.up;
+            PartInit(part);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))  // Lower
+        {
+            rUpper.enabled = Upper.enabled = false;
+            rHand.enabled = Hand.enabled = false;
+            rLower.enabled = Lower.enabled = true;
+
+            part = Part.low;
+            PartInit(part);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))  // Hand
+        {
+            rUpper.enabled = Upper.enabled = false;
+            rLower.enabled = Lower.enabled = false;
+            rHand.enabled = Hand.enabled = true;
+
+            part = Part.hand;
+            PartInit(part);
+        }
+    }
+}
--- MAD/Assets/Scripts/Miguel/RotateThatThang.cs
+++ MAD/Assets/Scripts/Miguel/RotateThatThang.cs
@@ -6,20 +6,58 @@ public class RotateThatThang : MonoBehaviour
 {
     public GameObject thingToRotate;
     public float rotateSpeed = 50f;
+    public bool rightControl = false;     // for multiplier
+    public bool isUpper = false;        // for Vector3's
+
+    private float multiplier = 1;
+    private Vector3 w, s, a, d, e, q;
+
+
+    void Start()
+    {
+        Init();
+    }
+
+    public void Init()
+    {
+        if (rightControl)
+            multiplier = -1;
+        else
+            multiplier = 1;
+
+        if (isUpper)
+        {
+            w = Vector3.up * multiplier;
+            s = Vector3.down * multiplier;
+            a = Vector3.forward * multiplier;
+            d = Vector3.back * multiplier;
+            e = Vector3.left;
+            q = Vector3.right;
+        }
+        else
+        {
+            w = Vector3.left;
+            s = Vector3.right;
+            a = Vector3.up * multiplier;
+            d = Vector3.down * multiplier;
+            e = Vector3.forward * multiplier;
+            q = Vector3.back * multiplier;
+        }
+    }
 
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
-            thingToRotate.transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
-        else if (Input.GetKey(KeyCode.S))
-            thingToRotate.transform.Rotate(Vector3.down, rotateSpeed * Time.deltaTime);
-        else if (Input.GetKey(KeyCode.A))
-            thingToRotate.transform.Rotate(Vector3.forward, rotateSpeed * Time.deltaTime);
-        else if (Input.GetKey(KeyCode.D))
-            thingToRotate.transform.Rotate(Vector3.back, rotateSpeed * Time.deltaTime);
-        else if (Input.GetKey(KeyCode.Q))
-            thingToRotate.transform.Rotate(Vector3.left, rotateSpeed * Time.deltaTime);
-        else if (Input.GetKey(KeyCode.E))
-            thingToRotate.transform.Rotate(Vector3.right, rotateSpeed * Time.deltaTime);
+            thingToRotate.transform.Rotate(w, rotateSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            thingToRotate.transform.Rotate(s, rotateSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+            thingToRotate.transform.Rotate(a, rotateSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            thingToRotate.transform.Rotate(d, rotateSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.E))
+            thingToRotate.transform.Rotate(e, rotateSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.Q))
+            thingToRotate.transform.Rotate(q, rotateSpeed * Time.deltaTime);
     }
 }
