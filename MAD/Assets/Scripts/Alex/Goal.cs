ed2be1ffec5f3f80000018c569829f7d762acfb0
alexpena5635 <alexpena5635@gmail.com>
2022-02-11 00:34:04 -0800

    More work on p2p task

--- MAD/Assets/Scripts/Alex/Goal.cs
+++ MAD/Assets/Scripts/Alex/Goal.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
+    // Inspector-assigned event to be invoked when the goal is "reached"
     public GameEvent goalEvent;
+    public GameEvent goalReset;
     void OnTriggerEnter(Collider col)
     {
         // Debug.Log("Name is:[" + col.name + "]");
@@ -14,4 +16,13 @@ public class Goal : MonoBehaviour
             Debug.Log("[" + col.name + "] is touching the goal sphere!");
         }
     }
+
+    // void OnTriggerExit(Collider col)
+    // {
+    //     if(col.tag == "UserHand")
+    //     {
+    //         goalReset?.Raise();
+    //         // Debug.Log("[" + col.name + "] is leaving the goal sphere!");
+    //     }
+    // }
 }
--- MAD/Assets/Scripts/Alex/Tasks/TaskRunner.cs
+++ MAD/Assets/Scripts/Alex/Tasks/TaskRunner.cs
@@ -11,19 +11,34 @@ public class TaskRunner : MonoBehaviour
     // Local copy of the specific task config that was assigned in the inspector
     private TaskConfig taskConfigCopy;
 
-    void Start()
-    {
-        taskConfigCopy = Instantiate(taskConfig);
-    }
-
-    /*
+        /*
         Task runner should be able to have a list of events that we can add to it dynamically
         - list of events stored that we can add to or remove from dynamically
         - call/broadcast any one of them
         - subscribe a method to, or remove a method from them as well
     */ 
     // List of gameEvents for a specifc task
-    public List<GameEvent> gameEvents = new List<GameEvent>();
+    public List<GameEvent> gameEventsPublic = new List<GameEvent>();
+
+    private List<GameEvent> gameEvents = new List<GameEvent>();
+
+    void Start()
+    {
+        // Copy the task configuration
+        taskConfigCopy = Instantiate(taskConfig);
+
+        // Copy the events in the public GameEvent list to a private one
+        // (this way we can modify them without changing the S.O)
+        gameEvents?.Clear();
+        gameEvents.AddRange(gameEventsPublic);
+
+        
+        // TODO: Setup the objects in configuration
+    }
+
+
+    // TODO: Write main logic loop
+
 
     // Register/Add a new GameEvent to the list
     public void Register(GameEvent newEvent)
