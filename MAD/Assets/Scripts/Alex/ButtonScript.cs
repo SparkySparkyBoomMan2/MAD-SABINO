6b32e616bcaf08519f6c9d2cede508d3908b44c6
Dawson Hill <extremedawson@gmail.com>
2022-02-20 16:43:30 -0800

    Update to ButtonScript
     - Update to include correct TMPro Component type

--- MAD/Assets/Scripts/Alex/ButtonScript.cs
+++ MAD/Assets/Scripts/Alex/ButtonScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ButtonScript : MonoBehaviour
 {
@@ -13,7 +14,7 @@ public class ButtonScript : MonoBehaviour
         Button thisButton = GetComponent<Button>();
 
         // Set the text of the button to the name of the task
-        thisButton.GetComponentInChildren<Text>().text = task.taskName;
+        thisButton.GetComponentInChildren<TextMeshProUGUI>().SetText(task.taskName, true);
         
         // Set the onclick function of the button to load the task scene
         thisButton.onClick.AddListener(() => task.invokeTaskEvent());
