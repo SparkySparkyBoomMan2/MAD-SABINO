bb83ea2686f9b7438d3549dadaea5e9bbfff4b67
alexpena5635 <alexpena5635@gmail.com>
2022-03-29 10:03:49 -0700

    P2p changes, home position

---
+++ MAD/Assets/Scripts/Alex/Home.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Home : MonoBehaviour
+{
+    // Inspector-assigned event to be invoked when the goal is "reached"
+    public GameEvent homeEvent;
+    public GameEvent homeReset;
+    void OnTriggerEnter(Collider col)
+    {
+        // Debug.Log("Name is:[" + col.name + "]");
+        if(col.tag == "UserHand")
+        {
+            homeEvent?.Raise();
+            Debug.Log("[" + col.name + "] is touching the home sphere!");
+        }
+    }
+
+    // void OnTriggerExit(Collider col)
+    // {
+    //     if(col.tag == "UserHand")
+    //     {
+    //         homeReset?.Raise();
+    //         // Debug.Log("[" + col.name + "] is leaving the goal sphere!");
+    //     }
+    // }
+}
